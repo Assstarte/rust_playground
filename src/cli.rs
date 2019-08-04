04e0b9a6c9a1dd26a16fd7863b44bc84a2def574
Assstarte <max@ezic.io>
2019-08-04 22:24:32 +0300

    If command is not specified - abort to prevent panic

--- src/cli.rs
+++ src/cli.rs
@@ -2,6 +2,10 @@ use std::env;
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("Command not specified. Aborted.");
+        std::process::exit(0);
+    }
     let command = args[1].clone();
     let name = "Max";
     let status = "100%";
